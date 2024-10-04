18c47ceb3b4f1dbbeb10eded46a1f74190caeb33
rifavs <rifavs40@gmail.com>
2024-10-04 12:07:15 +0600

    Add files via upload

---
+++ mark.py
@@ -0,0 +1,9 @@
+s1=int(input("Enter the mark of first subject:"))
+s2=int(input("Enter the mark of second subject:"))
+s3=int(input("Enter the mark of third subject:"))
+s4=int(input("Enter the mark of forth subject:"))
+s5=int(input("Enter the mark of fifth subject:"))
+total=s1+s2+s3+s4+s5
+per=(total/500)*100
+print (total)
+print (per)
