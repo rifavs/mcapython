a4ed52fc8f3d126210dba9813d7140217861fce8
rifavs <rifavs40@gmail.com>
2024-10-23 11:28:55 +0530

    Add files via upload

---
+++ week3/color23-10-2024.py
@@ -0,0 +1,8 @@
+a=[]
+s=int(input("enter the size "))
+for i in range(s):
+    c=str(input("Enter the colors "))
+    a.append(c)
+print("them list of color ",a)
+print("first color in the list ",a[0])
+print("last color in the list ",a[-1])
